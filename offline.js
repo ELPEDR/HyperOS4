826ae79b34ebcf4b94aeb7e14aaf6a254c83b792
ELPEDR <101681387+ELPEDR@users.noreply.github.com>
2026-09-05 13:29:07 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1788625706,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/wallpaper-sheet0.png",
+		"images/back-sheet0.png",
+		"images/settings-sheet0.png",
+		"images/settings-sheet1.png",
+		"images/hourbig.png",
+		"images/spritefont2.png",
+		"images/openanim.png",
+		"images/ghosttouch-sheet0.png",
+		"images/blur-sheet0.png",
+		"images/bar-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
